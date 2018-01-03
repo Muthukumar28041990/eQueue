e8e8ea8dca40bdd98d8b143831047c02c646254b
Rajesh Babu <rajeshbabu@arivuventures.com>
2018-01-03 09:09:59 +0530

    home file updated

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.home',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var OnqueApp = angular.module('OnqueApp', ['ngRoute','ui.router']);
 
-  $routeProvider.otherwise({redirectTo: '/home'});
+OnqueApp.config(['$stateProvider', '$locationProvider', '$routeProvider', function($stateProvider, $locationProvider, $routeProvider) {
+  
+  $stateProvider.state('home', {
+		url : '/home',
+		templateUrl : 'modules/home/home.html',
+		controller : 'homeController'
+ });
+
+ $routeProvider.otherwise({redirectTo: '/home'});
+
 }]);
